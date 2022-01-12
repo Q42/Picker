fc85f435ae5735e29bdae057f86b6695f09efea5
Ben Lenarts <ben@q42.nl>
2022-01-12 16:50:35 +0100

    Update dependencies (#3)
    * Upgrade platform dependency to conform to the package dependency
    * Update Promissum dependency to v7.x

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@ import PackageDescription
 let package = Package(
   name: "Picker",
   platforms: [
-    .macOS(.v10_11), .iOS(.v10), .tvOS(.v9), .watchOS(.v3)
+    .macOS(.v10_12), .iOS(.v10), .tvOS(.v9), .watchOS(.v3)
   ],
   products: [
     .library(name: "Picker", targets: ["Picker"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/tomlokhorst/Promissum", from: "5.0.0")
+    .package(url: "https://github.com/tomlokhorst/Promissum", from: "7.0.0")
   ],
   targets: [
     .target(name: "Picker", dependencies: ["Promissum"]),
